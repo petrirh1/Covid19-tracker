73a4d24cab1ca7cf6c589ef35548f75151795a43
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2020-09-02 22:31:02 +0300

    Updates

---
+++ precache-manifest.55e1fbba0ef840d598657a5422f2e9f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1788337decd1127dea39c9e8fae024",
+    "url": "/Covid19-tracker/index.html"
+  },
+  {
+    "revision": "51b436ee7ead538e3fa2",
+    "url": "/Covid19-tracker/static/css/2.067ccc96.chunk.css"
+  },
+  {
+    "revision": "a202270c757756606d73",
+    "url": "/Covid19-tracker/static/css/main.8bc1a082.chunk.css"
+  },
+  {
+    "revision": "51b436ee7ead538e3fa2",
+    "url": "/Covid19-tracker/static/js/2.562cab0d.chunk.js"
+  },
+  {
+    "revision": "3ae6f1a901c2ba7ccf1880c0e6d406e1",
+    "url": "/Covid19-tracker/static/js/2.562cab0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a202270c757756606d73",
+    "url": "/Covid19-tracker/static/js/main.af07b7aa.chunk.js"
+  },
+  {
+    "revision": "2a70ba93b0464cd30908",
+    "url": "/Covid19-tracker/static/js/runtime-main.1cd7fd49.js"
+  }
+]);
